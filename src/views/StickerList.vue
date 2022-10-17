31505043b4b5b295a080a81ff3ae11438c7546c5
Rene Dolores <renecms@gmail.com>
2022-10-16 23:40:18 -0300

    created a table for stickers

--- src/views/StickerList.vue
+++ src/views/StickerList.vue
@@ -1,22 +1,54 @@
 <template>
+  <v-checkbox
+    v-model="showOwned"
+    label="Mostrar apenas figurinhas não obtidas"
+  />
   <v-table>
     <thead>
       <tr>
+        <th
+          class="text-left"
+          width="150"
+        >
+          Figurinha
+        </th>
         <th class="text-left">
           Name
         </th>
         <th class="text-left">
-          Calories
+          Number
         </th>
+        <th class="text-left" />
+        <th class="text-left" />
       </tr>
     </thead>
     <tbody>
       <tr 
-        v-for="item in desserts"
+        v-for="item in listOfStickers"
         :key="item.name"
       >
+        <td class="align-center ">
+          <v-img
+            width="140"
+            height="200"
+            :src="item.imageUrl"
+          />
+        </td>
         <td>{{ item.name }}</td>
-        <td>{{ item.calories }}</td>
+        <td>{{ item.number }}</td>
+        <td>
+          <v-checkbox
+            v-model="item.wishlisted"
+            :disabled="item.owned"
+            label="Quero"
+          />
+        </td>
+        <td>
+          <v-checkbox
+            v-model="item.owned"
+            label="Tenho para trocar"
+          />
+        </td>
       </tr>
     </tbody>
   </v-table>
@@ -26,22 +58,37 @@
 export default {
   data() {
     return {
-      desserts: [
+      showOwned: false,
+      stickers: [
         {
-          name: 'Frozen Yogurt',
-          calories: 159,
+          name: 'Cristiano Ronaldo',
+          number: 7,
+          imageUrl: "https://inmagazine.ig.com.br/public/assets/img/galeria-imagens/CR7.jpg",
+          wishlisted: false,
+          owned: true
         },
         {
-          name: 'Ice cream sandwich',
-          calories: 237,
+          name: 'Lionel Messi',
+          number: 10,
+          imageUrl: "https://http2.mlstatic.com/D_NQ_NP_758194-MLB51370916676_092022-W.jpg",
+          wishlisted: false,
+          owned: true
         },
         {
-          name: 'Eclair',
-          calories: 262,
+          name: 'Neymar',
+          number: 10,
+          imageUrl: "https://imagens.ne10.uol.com.br/veiculos/_midias/png/2022/08/29/figurinha_dourada_de_neymar-21628926.png",
+          wishlisted: false,
+          owned: false
         }
       ],
     }
   },
+  computed: {
+    listOfStickers() {
+      return this.showOwned ? this.stickers.filter(sticker => sticker.owned != this.showOwned) : this.stickers;
+    }
+  }
 }
 </script>
 
