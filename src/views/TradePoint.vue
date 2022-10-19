f60e7bc51b483cd0311c1c35e7405aeb4dd94222
Rene Dolores <renecms@gmail.com>
2022-10-19 00:47:39 -0300

    created trade points page

--- src/views/TradePoint.vue
+++ src/views/TradePoint.vue
@@ -1,10 +1,99 @@
 <template>
-  Trade Point
+  <v-row>
+    <v-col cols="5">
+      <v-text-field
+        v-model="searchTerm"
+        label="Busca por nome do ponto de troca"
+      />
+    </v-col>
+    <v-col cols="3">
+      <v-checkbox
+        v-model="showLinkedOnly"
+        label="Mostrar apenas pontos frequentados"
+      />
+    </v-col>
+  </v-row>
+
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">
+          Name
+        </th>
+        <th class="text-left">
+          Endereço
+        </th>
+        <th class="text-left" />
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in listOfTradePoints"
+        :key="item.id"
+      >
+        <td>{{ item.name }}</td>
+        <td>{{ item.address }}</td>
+        <td>
+          <v-checkbox
+            v-model="item.linked"
+            label="Ponto frequentado"
+            @change="linkedChange(item)"
+          />
+        </td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
 
-<script  setup lang="ts">
 
+<script lang="ts">
+
+interface TradePoint {
+  id: number;
+  name: string;
+  address: string;
+  linked: boolean;
+}
+export default {
+  data() {
+    return {
+      showLinkedOnly: false,
+      searchTerm: "",
+      tradePoints: new Array() as Array<TradePoint>,
+    }
+  },
+  computed: {
+    listOfTradePoints(): Array<TradePoint> {
+      this.tradePoints
+      const filterLinked = (list: Array<TradePoint>) => {
+        return this.showLinkedOnly ? list.filter(sticker => sticker.linked != this.showLinkedOnly) : list
+      }
+      const filterName = (list: Array<TradePoint>) => {
+        return this.searchTerm ? list.filter(sticker => sticker.name.toLocaleLowerCase().includes(this.searchTerm.toLocaleLowerCase())) : list;
+      }
+      return filterName(filterLinked(this.tradePoints));
+    }
+  },
+  mounted() {
+    fetch("https://stickers-trade-be-vqklpjxjja-rj.a.run.app/trade_point")
+      .then(response => response.json())
+      .then(data => this.tradePoints = data);
+
+  },
+  methods: {
+    linkedChange(item: TradePoint) {
+      console.log(item);
+
+    }
+  }
+
+}
 </script>
+
+<style>
+
+</style>
+
 
 <style>
 </style>
